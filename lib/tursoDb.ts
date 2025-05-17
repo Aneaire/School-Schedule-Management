e35fecd303c130db33ae657eed92fc976492bfac
Aneaire <aneaire010@gmail.com>
2025-05-17 12:52:06 +0800

    new db

--- lib/tursoDb.ts
+++ lib/tursoDb.ts
@@ -3,9 +3,9 @@ import { drizzle } from "drizzle-orm/libsql";
 import { schema } from "./schema";
 
 const client = createClient({
-  url: "libsql://scheduler-aneaire.aws-ap-northeast-1.turso.io",
+  url: "libsql://school-scheduler-aneaire.aws-ap-northeast-1.turso.io",
   authToken:
-    "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3NDc0NTE5MzQsImlkIjoiYjk3OTUyZjgtMzA4Ni00ZWE4LTliYWMtNmNiODBhOTU3NjgyIiwicmlkIjoiMTY4NDI5NjktMjE1Ny00MWM4LThlNTMtNDU3NjkyOTY3OTIzIn0.W5KSFHyox-m0SNsaN5mMpPYbXThGlVjxhYLAs_u8RkWGaP6ElrJqWl_eDIiP1ufp5rZDvGAiBHuIjk_TofZGDA",
+    "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3NDc0NTY2NjAsImlkIjoiMTJmMzBkYTktZjFlNS00MDAzLWJjNzctMzgwM2E0NGYyYzAwIiwicmlkIjoiNTQ5MzhlZTQtZmJkNC00N2M3LWJmYjUtOTE4ZGM3ZjI1Y2FmIn0.pUPDxIzrpTC-IfYk7cxSp4tMfvumHq_bNHS-MqTdnsLAbt_8t2F9PN_al82J8MmiE_S36g7bSt7vZ_Q6pq_wBQ",
 });
 
 export const db = drizzle(client, { schema });
